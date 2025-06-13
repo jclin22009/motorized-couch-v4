2e773e3194a75c1f5ef96439a772bc8474137fb6
Jason Lin <jclin2.2009@gmail.com>
2025-06-12 18:00:42 -0700

    simple motor control code

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from motorized-couch-v4!")
+import asyncio
+import odrive
+from odrive.enums import AxisState
 
+async def find_device():
+    odrv0 = await odrive.find_async()
+    await odrv0.axis0.requested_state.write(AxisState.CLOSED_LOOP_CONTROL)
+    return odrv0
 
-if __name__ == "__main__":
-    main()
+async def set_speed(speed, device):
+    await device.axis0.controller.input_vel.write(speed)
+
+async def main():
+    odrv0 = await find_device()
+    await set_speed(0, odrv0)
+
+asyncio.run(main())
